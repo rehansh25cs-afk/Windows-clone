ed5e973283922f1211107a2c67d81e5282ec737d
Rehan Shaikh <rehandev3@gmail.com>
2025-12-15 13:21:36 +0530

    added more functionalities and shii

--- sctipt.js
+++ sctipt.js
@@ -1,5 +1,7 @@
 let context = document.querySelector('.contextmenu')
 
+let start = document.querySelector('.start-menu')
+
 let flag = 0;
 document.body.addEventListener('contextmenu', (dets) => {
     dets.preventDefault()
@@ -9,8 +11,27 @@ document.body.addEventListener('contextmenu', (dets) => {
 
 
 
+
 })
 
-document.body.addEventListener('click', (dets) => { 
-    context.style.display = 'none';
+document.querySelector('.windows').addEventListener('click', (e) => {
+    console.log('hello');
+    e.stopPropagation(); 
+   
+    if (flag === 0) {
+        start.style.bottom = '10' + '%'
+        flag = 1;
+    } else if(flag = 1) {
+        start.style.bottom = '-100' + '%'
+        flag = 0
+    }
 })
+
+document.body.addEventListener('click', (dets) => {
+    context.style.display = 'none';
+    if (flag === 1) {
+        start.style.bottom = '-100' + '%'
+        flag = 0
+    }
+})
+
